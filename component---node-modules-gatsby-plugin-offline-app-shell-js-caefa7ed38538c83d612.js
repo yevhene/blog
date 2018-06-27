1a6278071a1f7164b425046e6cbbc4615d3fea6b
Yevhen Shemet <yevhene@gmail.com>
2018-06-27 20:23:36 +0300

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-caefa7ed38538c83d612.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{220:function(e,t,n){"use strict";var u=n(5);t.__esModule=!0,t.default=void 0;var o=u(n(33)),r=u(n(1)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement("div",null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-caefa7ed38538c83d612.js.map
